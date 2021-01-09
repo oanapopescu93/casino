36644fbc79d9cc8f5070b3313f5c2c67bc114e60
oanapopescu93 <oanapopescu93@gmail.com>
2021-01-09 20:25:49 +0200

    game and user account visible

--- client/src/components/pages/userPage.js
+++ client/src/components/pages/userPage.js
@@ -16,22 +16,40 @@ const socket = socketIOClient("/");
 var self;
 var visible = false;
 
-function Child(props) {
-	var user_id = props.user_id;
-	var user = props.user;
-	var url = props.url;
-	var money = props.money;
-	var type = props.type;
-	var user_table = props.user_table;
-	var game = props.game;
+class Child extends Component {
+	constructor(props) {
+		super(props);
+		self = this;
 
-	function handleClick(link) {
+		this.handleClick = this.handleClick.bind(self);
+
+		this.state = {
+			visible: false,
+			user_id: props.user_id,
+			user: props.user,
+			url: props.url,
+			money: props.money,
+			type: props.type,
+			user_table: props.user_table,
+			game: props.game,
+		}; 
+	}
+	
+	componentDidMount() {
+		$(window).click(function(e) {		
+			if(e.target.id !== "user_icon_path" && e.target.id !== "user_icon" && e.target.id !== "user_details"){
+				$('.user_details_container').removeClass('open')
+			} 
+		});
+	}
+
+	handleClick(link) {
 		switch (link) {
 			case "account":
-				visible = true;
+				this.setState({ visible: true });
 			  	break;
 			case "casino":
-				visible = false;
+				this.setState({ visible: false });
 				break;
 			case "user_icon":
 				$('.user_details_container').toggleClass('open');
@@ -42,55 +60,48 @@ function Child(props) {
 		  }
 	}
 
-	$(window).click(function(e) {		
-		if(e.target.id !== "user_icon_path" && e.target.id !== "user_icon" && e.target.id !== "user_details"){
-			$('.user_details_container').removeClass('open')
-		} 
-	});
-
-	return (			
-			<BrowserRouter>
-				<div className="userPage"> 
-					<Navbar bg="light" expand="lg">
-						<div className="logo_title">
-							<Navbar.Brand href="/">
-								<img alt="logo_icon" className="logo_title_img" src={logo_icon} />
-								<div className="logo_title_text">BunnyBet</div>
-							</Navbar.Brand>							
+	render() {
+		return (			
+				<BrowserRouter>
+					<div className="userPage"> 
+						<Navbar bg="light" expand="lg">
+							<div className="logo_title">
+								<Navbar.Brand href="/">
+									<img alt="logo_icon" className="logo_title_img" src={logo_icon} />
+									<div className="logo_title_text">BunnyBet</div>
+								</Navbar.Brand>							
+							</div>
+						</Navbar>
+						<div id="user_money" className="user_money">
+							<div id="nav_money"><span>{this.state.money}</span><img alt="carrot_img" className="currency_img" src={carrot_img} /></div>
 						</div>
-					</Navbar>
-					<div id="user_money" className="user_money">
-						<div id="nav_money"><span>{money}</span><img alt="carrot_img" className="currency_img" src={carrot_img} /></div>
+						<div id="user_details" className="user_details" >
+							<svg onClick={()=>self.handleClick('user_icon')} id="user_icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-circle" className="svg-inline--fa fa-user-circle fa-w-16" role="img" viewBox="0 0 496 512">
+								<path id="user_icon_path" fill="yellow" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"/>
+							</svg>
+							<div className="user_details_container">
+								<div className="user_details_box">								
+									<div id="nav_user" className="nav_user" onClick={()=>self.handleClick('casino')}>{this.state.user}</div>
+									<div id="nav_account" className="nav_user" onClick={()=>self.handleClick('account')}>My account</div>
+									<div id="nav_logout" className="nav_user" onClick={()=>self.handleClick('logout')}>Logout</div>
+								</div>	
+							</div>						
+						</div>
+						<Row>
+							<Col sm={12}>
+								
+										{ !this.state.visible ? 
+											<Game user_id={this.state.user_id} game={this.state.game} user={this.state.user} money={this.state.money} user_table={this.state.user_table} type={this.state.type} socket={socket}></Game>									
+											: 
+											<UserAccount user={this.state.user} money={this.state.money} user_table={this.state.user_table} type={this.state.type}></UserAccount> 
+										}						
+							</Col>
+						</Row>	
 					</div>
-					<div id="user_details" className="user_details" >
-						<svg onClick={()=>handleClick('user_icon')} id="user_icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-circle" className="svg-inline--fa fa-user-circle fa-w-16" role="img" viewBox="0 0 496 512">
-							<path id="user_icon_path" fill="yellow" d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"/>
-						</svg>
-						<div className="user_details_container">
-							<div className="user_details_box">								
-								<div id="nav_user" className="nav_user" onClick={()=>handleClick('casino')}>{user}</div>
-								<div id="nav_account" className="nav_user" onClick={()=>handleClick('account')}>My account</div>
-								<div id="nav_logout" className="nav_user" onClick={()=>handleClick('logout')}>Logout</div>
-							</div>	
-						</div>						
-					</div>
-					<Row>
-						<Col sm={12}>
-							<Switch>			
-								<Route path={url}>
-									{ !visible ? 
-										<Game user_id={user_id} game={game} user={user} money={money} user_table={user_table} type={type} socket={socket}></Game>									
-										: 
-										<UserAccount user={user} money={money} user_table={user_table} type={type}></UserAccount> 
-									}
-								</Route>
-							</Switch>							
-						</Col>
-					</Row>	
-				</div>
-			</BrowserRouter>
-		);
-  }
+				</BrowserRouter>
+			);
+	}
+}
 
 class UserPage extends Component {
 	constructor(props) {
