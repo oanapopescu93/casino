18f94a6b0c2a77014c6e73148f1d13bd007d9274
Oana Popescu <oana.popescu@idriveglobal.com>
2021-02-08 14:49:18 +0200

    heroku

--- var/constants.js
+++ var/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    SERVER: "https://casino00.herokuapp.com",
+    SERVER: "http://localhost:3000",
     AUTH_USER: "09f009efe7b9ae",
     AUTH_PASS: "cd1233a2d8d6a7",
     AUTH_FROM: "oanapopescu93@gmail.com",
