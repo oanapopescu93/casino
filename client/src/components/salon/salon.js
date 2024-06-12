e4d3d5196005734fd7097beed02140e3cc108a62
Oana Popescu <oana.popescu@idriveglobal.com>
2024-06-12 15:46:33 +0300

    page_exit

--- client/src/components/salon/salon.js
+++ client/src/components/salon/salon.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import { translate } from '../../translations/translate'
-import { setCookie } from '../../utils/utils'
 import Header from '../partials/header'
 import SalonGames from './salonGames'
 import SalonSidebarLeft from './salonSidebarLeft'
@@ -15,11 +12,6 @@ function Salon(props) {
     const {lang, home, page} = props 
     let dispatch = useDispatch()
 
-    function handleExit(){
-        setCookie('casino_uuid', '')
-        window.location.reload(false)
-    }
-
     function handleWhack(){
         dispatch(changePage('Salon'))
         dispatch(changeGamePage(null))
@@ -31,12 +23,7 @@ function Salon(props) {
             <Language title={lang}></Language>          
             <div className="content_wrap">                
                 <Header template="salon" lang={lang}></Header>
-                <SalonGames lang={lang} items={home.products}></SalonGames>
-                <div className="page_exit">
-                    <Button id="exit_salon" type="button" onClick={()=>handleExit()} className="mybutton button_transparent shadow_convex">
-                        {translate({lang: lang, info: "exit_salon"})}
-                    </Button>
-                </div>
+                <SalonGames lang={lang} items={home.products}></SalonGames>                
             </div>
             <SalonSidebarLeft lang={lang}></SalonSidebarLeft>
             <Panel {...props}></Panel>
